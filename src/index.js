12ec26613536c1517ce5fb44a91bf45d7cbdd1f2
Colonna, Olivier <olivier.colonna@accenture.com>
2016-01-28 00:58:13 +0000

    travis CI setup

---
+++ src/index.js
@@ -0,0 +1,30 @@
+/* jshint strict: true */
+/* jshint esversion: 6 */
+
+/**
+ * @author oliviercolonna@gmail.com
+ * Copyright(c) 2016 Olivier Colonna
+ * MIT Licensed
+ */
+
+import React from 'react';
+
+class ReactList extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+};
+
+ReactList.defaultProps = {};
+
+ReactList.propTypes = {};
+
+export default ReactList;
